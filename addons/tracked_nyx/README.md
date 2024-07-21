71a1e678dba27fa8cf85ec113d812eb5ee27b384
erem2k <nope@nohost.com>
2024-07-21 19:56:36 +0200

    tracked_nyx - add HAT configuration, courtesy of blah2355

--- addons/tracked_nyx/README.md
+++ addons/tracked_nyx/README.md
@@ -1,1 +1,6 @@
 # AWC Nyx, Tracked, Light Tank
+Adds additional variants of AWC Nyx light tank
+
+## Heavy Anti-Tank
+Adds a heavy anti-tank variant of the Nyx under the AAF. Uses laser guided missiles (similar to the Rhino MGS).
+Courtesy of blah2355.
--- addons/tracked_nyx/config.cpp
+++ addons/tracked_nyx/config.cpp
@@ -3,12 +3,22 @@
 class CfgPatches {
 	class ADDON {
 		name = COMPONENT_NAME;
-		units[] = {};
-		weapons[] = {};
-		requiredVersion = REQUIRED_VERSION;
-		requiredAddons[] = {
-			"tmtm_vehicles_main"
+		units[] = {
+			"TMTM_I_LT_01_HAT_F"
 		};
+		weapons[] =	{
+			"TMTM_LAHAT"
+		};
+		magazines[] = {
+			"2Rnd_120mm_LG_missiles"
+		};
+		requiredAddons[] = {
+			"tmtm_vehicles_main",
+			"A3_Armor_F_Tank_LT_01"
+		};
+
+		requiredVersion = REQUIRED_VERSION;
+		
 		author = MOD_AUTHOR;
 		url = MOD_URL;
 		version = VERSION;
@@ -17,4 +27,7 @@ class CfgPatches {
 	};
 };
 
+#include "CfgAmmo.hpp"
+#include "CfgMagazines.hpp"
+#include "CfgWeapons.hpp"
 #include "CfgVehicles.hpp"
